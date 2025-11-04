b860841d8d18cc4d9069e57859df2b3c0e4d7be8
butool32 <138138778+butool32@users.noreply.github.com>
2025-11-04 09:14:22 +0530

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,30 @@
+# Terraform Block
+
+terraform {
+
+  required_version = ">= 0.14, <= 2.0.0" # Allows OpenTofu versions from 0.14 up to 2.0
+
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 3.0"  # Allows AWS provider versions >= 3.0 and < 4.0
+
+    }
+
+  }
+
+}
+ 
+# Provider Block
+
+provider "aws" {
+
+  region  = var.aws_region
+
+  profile = "default"
+
+}
+ 
