4f4b8ff5bff649b7aacdb8e02f9c9698f5d141a5
butool32 <138138778+butool32@users.noreply.github.com>
2025-11-04 09:23:39 +0530

    Update vpc-output.tf

--- vpc-output.tf
+++ vpc-output.tf
@@ -1,72 +1,37 @@
-# VPC Input Variables
+# VPC Output Values
 
-# VPC Name
-variable "vpc_name" {
-  description = "VPC Name"
-  type = string 
-  default = "myvpc"
+# VPC ID
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
 }
 
-# VPC CIDR Block
-variable "vpc_cidr_block" {
-  description = "VPC CIDR Block"
-  type = string 
-  default = "10.0.0.0/16"
-}
-
-# VPC Availability Zones
-variable "vpc_availability_zones" {
-  description = "VPC Availability Zones"
-  type = list(string)
-  default = ["us-east-1a", "us-east-1b"]
-}
-
-# VPC Public Subnets
-variable "vpc_public_subnets" {
-  description = "VPC Public Subnets"
-  type = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
+# VPC CIDR blocks
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
 }
 
 # VPC Private Subnets
-variable "vpc_private_subnets" {
-  description = "VPC Private Subnets"
-  type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
 }
 
-# VPC Database Subnets
-variable "vpc_database_subnets" {
-  description = "VPC Database Subnets"
-  type = list(string)
-  default = ["10.0.151.0/24", "10.0.152.0/24"]
+# VPC Public Subnets
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
 }
 
-# VPC Create Database Subnet Group (True / False)
-variable "vpc_create_database_subnet_group" {
-  description = "VPC Create Database Subnet Group"
-  type = bool
-  default = true 
+# VPC NAT gateway Public IP
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value       = module.vpc.nat_public_ips
 }
 
-# VPC Create Database Subnet Route Table (True or False)
-variable "vpc_create_database_subnet_route_table" {
-  description = "VPC Create Database Subnet Route Table"
-  type = bool
-  default = true   
-}
-
-  
-# VPC Enable NAT Gateway (True or False) 
-variable "vpc_enable_nat_gateway" {
-  description = "Enable NAT Gateways for Private Subnets Outbound Communication"
-  type = bool
-  default = true  
-}
-
-# VPC Single NAT Gateway (True or False)
-variable "vpc_single_nat_gateway" {
-  description = "Enable only single NAT Gateway in one Availability Zone to save costs during our demos"
-  type = bool
-  default = true
+# VPC AZs
+output "azs" {
+  description = "A list of availability zones spefified as argument to this module"
+  value       = module.vpc.azs
 }
